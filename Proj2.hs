a7806bb99c9a33ea9500a1130f02f5caa44ccf64
Momotaro-Yvv <84366910+Momotaro-Yvv@users.noreply.github.com>
2022-05-17 02:12:35 +1000

    Complete feedback function
    also add generateAllLocations functions to get all possible Just type positions
    TODO: finish nextGuess using recursion, and prune nodes if possible\ refer to Sudoku

--- Proj2.hs
+++ Proj2.hs
@@ -10,12 +10,14 @@ This file includess main functions to make guesses and receive feedback for the 
 module Proj2 (Location,
             toLocation,
             fromLocation,
-            -- feedback,
+            feedback,
             GameState,
-            -- initialGuess,
+            initialGuess,
             -- nextGuess
             ) where
 import Data.List
+import Data.Char
+
 
 {-
 Type Declaritation
@@ -24,24 +26,42 @@ type Location = (Char, Char)
 -- GameState
 type GameState = ()
 
-
 {-
 Helper Functions
 -}
 
 
 countCorrect:: [Location] -> [Location] -> Int
-countCorrect guess target = countCorrect' (sort guess) (sort target)
+countCorrect guesses targets = countCorrect' (sort guesses) (sort targets)
 countCorrect' [] _ = 0
 countCorrect' _ [] = 0
-countCorrect' guess@(x: xs) target@(y: ys)
-    | x < y = countCorrect' xs target
+countCorrect' guesses@(x: xs) targets@(y: ys)
+    | x < y = countCorrect' xs targets
     | x == y = countCorrect' xs ys + 1
-    | otherwise = countCorrect' target ys
-
+    | otherwise = countCorrect' guesses ys
 
 count_1and2::[Location] -> [Location] -> (Int,Int)
-count_1and2 =
+count_1and2 [] _ = (0,0)
+count_1and2 (x:xs) targets =
+    addTuple (toClosestDis x targets) (count_1and2 xs targets)
+
+addTuple:: (Int, Int) -> (Int, Int) -> (Int, Int)
+addTuple (x1,x2) (y1,y2) = (x1+y1, x2+y2)
+
+calculateDistant:: Location -> Location -> Int
+calculateDistant (a,b) (c,d) = max (abs (x1 - x2)) (abs (y1 -y2))
+    where   x1 = ord a
+            x2 = ord c
+            y1 = ord b
+            y2 = ord d
+
+toClosestDis:: Location -> [Location] -> (Int, Int)
+toClosestDis oneGuess targets = case minimum ([calculateDistant oneGuess y| y <- targets]) of
+        1 -> (1,0)
+        2 -> (0,1)
+        _ -> (0,0)
+
+
 {-
 Main Functions
 -}
@@ -52,9 +72,9 @@ Main Functions
 toLocation :: String -> Maybe Location
 toLocation str
     | length str /= 2             = Nothing
-    | fst `notElem` "ABCDEFG"     = Nothing
+    | fst `notElem` "ABCDEFGH"     = Nothing
     | scd `notElem` "1234"        = Nothing
-    | otherwise                         = Just (fst,scd)
+    | otherwise                   = Just (fst,scd)
     where
         fst = head str
         scd = str !! 1
@@ -70,13 +90,30 @@ fromLocation (x1,x2) = x1 : [x2]
 
 -- feedback
 feedback :: [Location] -> [Location] -> (Int,Int,Int)
-feedback [x1,x2,x3] [y1,y2,y3] = (correct, dist_1, dist_2)
+feedback guesses targets = (correct, dist_1, dist_2)
     where
-        correct = countCorrect [x1,x2,x3] [y1,y2,y3]
-        (dist_1, dist_2)  = count_1and2 [x1,x2,x3] [y1,y2,y3]
+        correct = countCorrect guesses targets
+        (dist_1, dist_2) = count_1and2 guesses targets
+
+--test case
+-- feedback [('B','3'),('C','3'), ('H','3')] [('H','1'),('B','2'),('D','3')]
+-- feedback [('H','2'),('H','1'), ('B','2')] [('H','1'),('B','2'),('D','3')]
+-- feedback [('A','3'),('D','2'), ('H','1')] [('A','1'),('D','2'),('B','3')]
+-- feedback [('H','4'),('G','3'), ('H','2')] [('A','1'),('D','2'),('B','3')]
+-- feedback [('D','2'),('B','3'), ('A','1')] [('A','1'),('D','2'),('B','3')]
+-- H1, B2, D3	B1, A2, H3	0, 2, 1
+-- H1, B2, D3	B2, H2, H1	2, 1, 0
+-- A1, D2, B3	A3, D2, H1	1, 1, 0s
+-- A1, D2, B3	H4, G3, H2	0, 0, 0
+-- A1, D2, B3	D2, B3, A1	3, 0, 0
+
+generateAllLocations::[Maybe Location]
+generateAllLocations = [toLocation (x : [y]) | x <- "ABCDEFGH", y <- "1234"]
 
 -- initialGuess
--- initialGuess :: ([Location],GameState)
+-- initialGuess :: [Location]
+initialGuess = take 3 generateAllLocations
 
 -- nextGuess
--- nextGuess :: ([Location],GameState) -> (Int,Int,Int) -> ([Location],GameState)
+nextGuess :: ([Location],GameState) -> (Int,Int,Int) -> ([Location],GameState)
+nextGuess

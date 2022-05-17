968a2eaf841fcbb368758e84205f9e535494b53c
Momotaro-Yvv <84366910+Momotaro-Yvv@users.noreply.github.com>
2022-05-17 21:13:34 +1000

    C3

--- Proj2.hs
+++ Proj2.hs
@@ -13,18 +13,25 @@ module Proj2 (Location,
             feedback,
             GameState,
             initialGuess,
-            -- nextGuess
+            nextGuess
             ) where
+
 import Data.List
 import Data.Char
+import Data.Array
 
 
 {-
 Type Declaritation
 -}
+type Guesses = [Location]
 type Location = (Char, Char)
--- GameState
-type GameState = ()
+
+-- GameState,containing all the possible choices from which nextGuess can choose
+type GameState = Int
+
+-- the list of all possible Locations
+type Choices = [Location]
 
 {-
 Helper Functions
@@ -32,18 +39,18 @@ Helper Functions
 
 
 countCorrect:: [Location] -> [Location] -> Int
-countCorrect guesses targets = countCorrect' (sort guesses) (sort targets)
+countCorrect targets guesses = countCorrect' (sort targets) (sort guesses)
 countCorrect' [] _ = 0
 countCorrect' _ [] = 0
-countCorrect' guesses@(x: xs) targets@(y: ys)
-    | x < y = countCorrect' xs targets
-    | x == y = countCorrect' xs ys + 1
-    | otherwise = countCorrect' guesses ys
+countCorrect' targets@(y: ys) guesses@(x: xs)
+    | x < y = countCorrect' targets xs
+    | x == y = countCorrect' ys xs + 1
+    | otherwise = countCorrect' ys guesses
 
 count_1and2::[Location] -> [Location] -> (Int,Int)
-count_1and2 [] _ = (0,0)
-count_1and2 (x:xs) targets =
-    addTuple (toClosestDis x targets) (count_1and2 xs targets)
+count_1and2 _ [] = (0,0)
+count_1and2 targets (x:xs)=
+    addTuple (toClosestDis targets x) (count_1and2 targets xs)
 
 addTuple:: (Int, Int) -> (Int, Int) -> (Int, Int)
 addTuple (x1,x2) (y1,y2) = (x1+y1, x2+y2)
@@ -55,12 +62,25 @@ calculateDistant (a,b) (c,d) = max (abs (x1 - x2)) (abs (y1 -y2))
             y1 = ord b
             y2 = ord d
 
-toClosestDis:: Location -> [Location] -> (Int, Int)
-toClosestDis oneGuess targets = case minimum ([calculateDistant oneGuess y| y <- targets]) of
+toClosestDis:: [Location] -> Location -> (Int, Int)
+toClosestDis targets oneGuess = case minimum ([calculateDistant oneGuess y| y <- targets]) of
         1 -> (1,0)
         2 -> (0,1)
         _ -> (0,0)
 
+
+generateAllChoices :: [Maybe Location]
+generateAllChoices = [toLocation (x : [y]) | x <- "ABCDEFGH", y <- "1234"]
+
+-- Every set contains a unique empty subset.
+subsets :: (Eq t, Num t) => t -> [a] -> [[a]]
+subsets 0 _ = [[]]
+subsets _ [] = []
+subsets n (x : xs) = map (x :) (subsets (n - 1) xs) ++ subsets n xs
+
+--generate all possible choices of the guess
+choices:: [[Maybe Location]]
+choices = subsets 3 generateAllChoices
 
 {-
 Main Functions
@@ -79,7 +99,6 @@ toLocation str
         fst = head str
         scd = str !! 1
 
-
 -- fromLocation
 -- gives back the two-character string version of the specified location;
 -- for any location loc, toLocation (fromLocation loc) should return Just loc.
@@ -90,30 +109,36 @@ fromLocation (x1,x2) = x1 : [x2]
 
 -- feedback
 feedback :: [Location] -> [Location] -> (Int,Int,Int)
-feedback guesses targets = (correct, dist_1, dist_2)
+feedback targets guesses = (correct, dist_1, dist_2)
     where
-        correct = countCorrect guesses targets
-        (dist_1, dist_2) = count_1and2 guesses targets
+        correct = countCorrect targets guesses
+        (dist_1, dist_2) = count_1and2 targets guesses
 
 --test case
--- feedback [('B','3'),('C','3'), ('H','3')] [('H','1'),('B','2'),('D','3')]
--- feedback [('H','2'),('H','1'), ('B','2')] [('H','1'),('B','2'),('D','3')]
--- feedback [('A','3'),('D','2'), ('H','1')] [('A','1'),('D','2'),('B','3')]
--- feedback [('H','4'),('G','3'), ('H','2')] [('A','1'),('D','2'),('B','3')]
--- feedback [('D','2'),('B','3'), ('A','1')] [('A','1'),('D','2'),('B','3')]
+-- feedback [('H','1'),('B','2'),('D','3')] [('B','3'),('C','3'), ('H','3')]
+-- feedback [('H','1'),('B','2'),('D','3')] [('H','2'),('H','1'), ('B','2')]
+-- feedback [('A','1'),('D','2'),('B','3')] [('A','3'),('D','2'), ('H','1')]
+-- feedback [('A','1'),('D','2'),('B','3')] [('H','4'),('G','3'), ('H','2')]
+-- feedback [('A','1'),('D','2'),('B','3')] [('D','2'),('B','3'), ('A','1')]
+-- feedback [('A','2'),('A','1'), ('G','2')] [('H','1'),('G','2'),('H','4')]
 -- H1, B2, D3	B1, A2, H3	0, 2, 1
 -- H1, B2, D3	B2, H2, H1	2, 1, 0
--- A1, D2, B3	A3, D2, H1	1, 1, 0s
+-- A1, D2, B3	A3, D2, H1	1, 1, 0
 -- A1, D2, B3	H4, G3, H2	0, 0, 0
 -- A1, D2, B3	D2, B3, A1	3, 0, 0
 
-generateAllLocations::[Maybe Location]
-generateAllLocations = [toLocation (x : [y]) | x <- "ABCDEFGH", y <- "1234"]
-
 -- initialGuess
--- initialGuess :: [Location]
-initialGuess = take 3 generateAllLocations
+-- TODO: how to make smart initial guess?
+-- initialGuess :: [Maybe Location, GameState]
+initialGuess :: ([Maybe Location], GameState)
+initialGuess = (choices!! 3, 1)
 
 -- nextGuess
-nextGuess :: ([Location],GameState) -> (Int,Int,Int) -> ([Location],GameState)
-nextGuess
+-- nextGuess :: [Location,GameState] -> (Int,Int,Int) -> [Location,GameState]
+-- nextGuess (previouGuess, choices) (feedback previouGuess targets) = (previouGuess, choices) (feedback previouGuess targets) 1
+--     where newGuess =
+
+-- nextGuess :: [Maybe Location,GameState] -> (Int,Int,Int) -> [Maybe Location,GameState]
+-- nextGuess :: [Int] -> (a, b, c) -> [[Maybe Location]]
+-- nextGuess :: [a1] -> (a2, a3, a4) -> [[Maybe Location]]
+nextGuess (previouGuess,index) [a, b, c]  = (choices !! index, index+1)

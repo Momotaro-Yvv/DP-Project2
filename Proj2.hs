50c20f08ee264ea0070da51ceae9a8c1e30b05ff
Momotaro-Yvv <84366910+Momotaro-Yvv@users.noreply.github.com>
2022-05-19 22:39:55 +1000

    Final Submisson

--- Proj2.hs
+++ Proj2.hs
@@ -1,10 +1,29 @@
---Description
+-- COMP90048 Project2
+--Author: Yuwen Tao <yuwtao@student.unimelb.edu.au>
+--Student ID: 1183577
 
 {-
-This file includess main functions to make guesses and receive feedback for the battleship game,
-(Explain the problem), which is implemented by guessing battleship position on a grid
-(Overall approach to the problem)
-(important functions to note within the code)
+This file includess main functions of implementing a two-player guessing game.
+The game is played on a 4×8 grid, where the searcher need to guess the locations
+of 3 battleships hidden by the hider.
+After each guess, the hider responds with three numbers,
+which the searcher can use in next rounds.
+The game will end till all 3 battleships been found.
+
+
+There are two main functions used to implement this game:
+1. the "feedback" takes searcher's guess and hider's true location,
+    and return feedback of three number in a tuple
+
+    The main approach of this function is simply
+    calculating the distance of each guess and the target
+
+2. the "nextGuess" takes the Gamestate from the initialGuess,
+    and feedback from hider, and return the next guess, and new Gamestate
+
+    The main approach of this function is
+    1) selecting guesses with lowest expected number of remaining candidates
+    and 2) by pruning impossible candidate search space.
 -}
 
 module Proj2 (Location,
@@ -26,10 +45,8 @@ Type Declaritation
 -}
 type Guesses = [Location]
 type Location = (Char, Char)
-
 -- GameState,containing all the possible choices from which nextGuess can choose
-type GameState = ([[Location]],Int,[Location])
-
+type GameState = ([[Location]],[Location])
 -- the list of all possible Locations
 type Choices = [Location]
 
@@ -37,6 +54,8 @@ type Choices = [Location]
 Helper Functions
 -}
 
+-- This helper function takes two lists of Locations,
+--  and count how many (out of three) does the seacher guesses right
 countCorrect:: [Location] -> [Location] -> Int
 countCorrect targets guesses = countCorrect' (sort targets) (sort guesses)
 countCorrect' [] _ = 0
@@ -46,144 +65,193 @@ countCorrect' targets@(y: ys) guesses@(x: xs)
     | x == y = countCorrect' ys xs + 1
     | otherwise = countCorrect' ys guesses
 
+--This function calcuate and return a tuple:
+--(the total number of guesses 1 space away, guesses 2 space away)
 count_1and2::[Location] -> [Location] -> (Int,Int)
 count_1and2 _ [] = (0,0)
 count_1and2 targets (x:xs)=
     addTuple (toClosestDis targets x) (count_1and2 targets xs)
 
+-- This is a helper funtcion that add numbers of two tuple
+--  and output a new tuple
 addTuple:: (Int, Int) -> (Int, Int) -> (Int, Int)
 addTuple (x1,x2) (y1,y2) = (x1+y1, x2+y2)
 
-calculateDistant:: Location -> Location -> Int
-calculateDistant (a,b) (c,d) = max (abs (x1 - x2)) (abs (y1 -y2))
-    where   x1 = ord a
-            x2 = ord c
-            y1 = ord b
-            y2 = ord d
+-- This function calculate and return the distance of two Locations
+calculateDist:: Location -> Location -> Int
+calculateDist (a,b) (c,d) = max (abs (x1 - x2)) (abs (y1 -y2))
+    where
+        x1 = ord a
+        x2 = ord c
+        y1 = ord b
+        y2 = ord d
 
+-- This helper function takes the distance of two Locations,
+--    and tranfer into tuple format
 toClosestDis:: [Location] -> Location -> (Int, Int)
-toClosestDis targets oneGuess = case minimum ([calculateDistant oneGuess y| y <- targets]) of
+toClosestDis targets oneGuess =
+    case minimum ([calculateDist oneGuess y| y <- targets]) of
         1 -> (1,0)
         2 -> (0,1)
         _ -> (0,0)
 
+-- This helper function simple iterate all possible Location on the 8*4 grid
 generateAllChoices :: String -> String -> [Location]
-generateAllChoices col row= [fromJust (toLocation (x : [y])) | x <-col, y <- row]
+generateAllChoices col row =
+    [fromJust (toLocation (x : [y])) | x <-col, y <- row]
 
--- Every set contains a unique empty subset.
+-- This helper function choose every possible set for a given Locations
+--     and return them in a list
 chooseThree :: (Eq t, Num t) => t -> [a] -> [[a]]
 chooseThree 0 _ = [[]]
 chooseThree _ [] = []
 chooseThree n (x : xs) = map (x :) (chooseThree (n - 1) xs) ++ chooseThree n xs
 
---This function will take lists of Locations and the feedback, return a new list of pruned search space
--- pruningCorrectNum:: [Location] -> [Location] -> Int -> [Location]
-pruningCorrectNum :: (Eq a, Num a) => [Location] -> [[Location]] -> a -> [[Location]]
-pruningCorrectNum previouGuess originalSearchSpace correct_num =
+--This function will take lists of Locations and the feedback
+--  and the number of correct guess,
+--  if correct_num = 0, delete all sets that contain the previous 3 Locations,
+--  return a new list of pruned search space
+pruneCorrectNum :: (Eq a, Num a) => [Location] -> [[Location]] -> a -> [[Location]]
+pruneCorrectNum previouGuess originalSearchSpace correct_num =
     if correct_num == 0
         then
             filter (noDistanceOf 0 previouGuess) originalSearchSpace
     else originalSearchSpace
 
-pruningDistOneTwo:: [Location] -> [[Location]] -> Int-> Int -> Int -> [[Location]]
-pruningDistOneTwo previouGuess originalSearchSpace correct_num dist_1 dist_2 =
-    if dist_1 == 0 && dist_2 == 0
-            then filter (countDistanceOf [(1,0,0),(0,0,0),(2,0,0),(3,0,0)] previouGuess) originalSearchSpace
-        -- else
-        --     filter (countDistanceOf [(1,0,1),(1,0,2),(1,0,3),(2,0,1),(2,0,2),(2,0,3),(3,0,1),(3,0,2),(3,0,3)] previouGuess) originalSearchSpace
-    else originalSearchSpace
-
--- pruning_dist_2:: [Location] -> [[Location]] -> Int -> Int -> [[Location]]
--- pruning_dist_2 previouGuess originalSearchSpace dist_1 dist_2 =
---     if dist_1 == 0 &&  == 0
---         then filter (noDistanceOf 2 previouGuess) originalSearchSpace
---     else originalSearchSpace
--- ifDistanceHasOne [('D','4'),('H','1'),('E','4')] ('D','3')
-
-
-countDistanceOf::[(Int,Int,Int)] -> [Location] -> [Location] -> Bool
-countDistanceOf dist previouGuess thisSearchSpace=
-    feedback previouGuess thisSearchSpace `elem` dist
-
+-- Helper function for pruningCorrectNum
+-- Given two list of Locations,
+-- return true if none of them are overlap, and false otherwise
 noDistanceOf:: Int -> [Location] -> [Location] -> Bool
 noDistanceOf dist previouGuess thisSearchSpace=
-    dist `notElem` [calculateDistant x y| x <-previouGuess, y <- thisSearchSpace]
+    dist `notElem` [calculateDist x y| x <-previouGuess, y <- thisSearchSpace]
+
+-- It will take lists of Locations and feedback, numbers of the feedback
+--  if dist_1&dist_2 = 0, delete all sets within 2 spaces
+--  from the previous 3 Locations,
+--  if dist_1 =0, delete all the sets 1 space from the previous 3 Locations,
+--  and return a new list of pruned search space
+pruneDistOneTwo:: [Location] -> [[Location]] -> Int-> Int -> Int -> [[Location]]
+pruneDistOneTwo previouGuess searchSpace correct_num dist_1 dist_2 =
+    if dist_1 == 0 then
+        if dist_2 == 0
+            then filter (countDistanceOf [(0,0)] previouGuess) searchSpace
+        else
+            filter (countDistanceOf [(0,1),(0,2),(0,3)] previouGuess) searchSpace
+    else searchSpace
+
+-- Helper function for pruningDistOneTwo
+--  Given two lists of Locations,
+--  return true if the number of guesses 1 and 2 space away
+--  from a true target is the subset of dist, otherwise false
+countDistanceOf::[(Int,Int)] -> [Location] -> [Location] -> Bool
+countDistanceOf dist previouGuess thisSearchSpace=
+    count_1and2 previouGuess thisSearchSpace `elem` dist
+
+-- Helper function that divide two interger and keep the digits
+fractionalDivide :: (Fractional a1, Integral a2, Integral a3) => a2 -> a3 -> a1
+fractionalDivide a b = fromIntegral a / fromIntegral b
 
 {-
 Main Functions
 -}
 
--- toLocation
--- gives Just the Location named by the string, or Nothing if the string is not a valid location name.
+--Input: a string of 2 length
+--Output:
+    -- if valid, gives Just the Location named by the string,
+    -- is not a valid location name, Nothing
 toLocation :: String -> Maybe Location
 toLocation str
     | length str /= 2             = Nothing
-    | fst `notElem` "ABCDEFGH"     = Nothing
+    | fst `notElem` "ABCDEFGH"    = Nothing
     | scd `notElem` "1234"        = Nothing
-    | otherwise                   = Just (fst,scd)
+    | otherwise                   = Just (fst, scd)
     where
         fst = head str
         scd = str !! 1
 
--- fromLocation
--- gives back the two-character string version of the specified location;
--- for any location loc, toLocation (fromLocation loc) should return Just loc.
-    -- feedback [('A','2'),('B','2'),('C','2')] [('C','2'),('D','2'),('E','2')]
+-- Input: Location
+-- Output: two-character string of the specified location
 fromLocation :: Location -> String
-fromLocation (x1,x2) = x1 : [x2]
+fromLocation (x1, x2) = x1 : [x2]
 
--- feedback
-feedback :: [Location] -> [Location] -> (Int,Int,Int)
+-- Input: two Location respectively the searcher's guess and the ture targets
+-- Output the tuple:
+    -- (correct locations num, num of ship 1space away, num of 2spaces away)
+feedback :: [Location] -> [Location] -> (Int, Int, Int)
 feedback targets guesses = (correct, dist_1, dist_2)
     where
         correct = countCorrect targets guesses
         (dist_1, dist_2) = count_1and2 targets guesses
 
--- initialGuess
--- TODO: how to make smart initial guess?
+-- No input arguments
+-- Output:
+    -- initial guess (Location)
+    -- GameState: (the initial guess candicates, a potential list of targets)
 initialGuess :: ([Location], GameState)
-initialGuess = ([('A','1'),('A','2'),('B','1')], (initialSearchSpace, 0,[]))
-    where initialSearchSpace = chooseThree 3 $generateAllChoices "ABCDEFGH" "1234"
-
-nextGuess :: ([Location],GameState) -> (Int,Int,Int) -> ([Location],GameState)
-nextGuess (previouGuess, (originalGuessCandidates,index,confirmedLocation)) (correct_num, dist_1, dist_2) =
-    if previouGuess == [('A','1'),('A','2'),('B','1')]
-        if(correct_num, dist_1, dist_2) == (0,0,3) then
-            ([('H','4'),('H','3'),('G','4')],(initialPrunedSearchSpace,index+1,[('C','3')]))
-        else (correct_num, dist_1, dist_2) == (1,1,1)
-            ([('H','4'),('H','3'),('G','4')],(prunedSearchSpace,index+1,[('B','2')]))
-
-        (nextGuess, (prunedSearchSpace,index+1,newconfirmedLocation))
+initialGuess = ([('A','1'),('A','2'),('B','1')], (initialSearchSpace, []))
     where
-        prunedSearchSpace =
-            pruningDistOneTwo previouGuess (
-                pruningCorrectNum previouGuess (
-                    filter (/=previouGuess) originalGuessCandidates)
+        initialSearchSpace = chooseThree 3 $generateAllChoices "ABCDEFGH" "1234"
+
+-- Input: (guess of Location , GameState) from previous/initial round, feedback
+-- Output: new guess and updated GameState
+nextGuess :: ([Location], GameState) -> (Int, Int, Int) -> ([Location], GameState)
+nextGuess (previouGuess, (candidates,promisingLoc)) (correct_num, dist_1, dist_2) =
+    if dist_2 == 3 && previouGuess == [('A','1'),('A','2'),('B','1')]
+        --when from initial guess
+        then ([('H','4'),('H','3'),('G','4')],(initialPrune,[('C','3')]))
+    else
+        --when from previous guessing
+        (nextGuess, (prunedCandicts,newPromisingLoc))
+    where
+        prunedCandicts =
+            --if dis_1&dist_2 = 0,
+            -- prune all Locations around the previous location
+            pruneDistOneTwo previouGuess (
+                -- if correct_num =0 meaning none of the previous location is correct
+                pruneCorrectNum previouGuess (
+                    --delete the previous Location from candicate list
+                    filter (/=previouGuess) candidates)
                 correct_num
                 )
             correct_num dist_1 dist_2
 
-        initialPrunedSearchSpace = filter (/=previouGuess) $chooseThree 3 $('C','3') : ('D', '4') : generateAllChoices "EFGH" "1234"
-        newconfirmedLocation =
+        initialPrune = filter (/=previouGuess)
+            $chooseThree 3 $('C','3') : ('D', '4') : generateAllChoices "EFGH" "1234"
+
+        --update the potential list of targets
+        -- if previous round guess 2 or more locations correctly
+        newPromisingLoc =
             if correct_num ==2 then
-                confirmedLocation ++ previouGuess
+                promisingLoc ++ previouGuess
             else
-                confirmedLocation
-        possibilityListSorted = map (calculatePossibility prunedSearchSpace confirmedLocation) prunedSearchSpace
-        nextGuess = snd(minimum possibilityListSorted)
+                promisingLoc
 
+        --for each guess candidate, calculate its expected number
+        possibilityList = map (calcuPossibility prunedCandicts newPromisingLoc) prunedCandicts
+        --select the one with minimum expected number as next round guess
+        nextGuess = snd(minimum possibilityList)
 
-calculatePossibility :: Fractional a1 => [[Location]] -> [Location]-> [Location] -> (a1, [Location])
-calculatePossibility guessCandidates confirmedLocation guess=
-    if noDistanceOf 0 guess confirmedLocation then
+-- This function calculate expected number of each set of guess for "nextGuess",
+-- Input:
+    -- 1)a list of guess candicate of a guess,
+    -- 2)list of high possibility Locations,
+    -- 3)the guess whose value be calculated
+-- Output:
+    -- a tuple (the expected number, one guess)
+calcuPossibility :: Fractional a1 => [[Location]] -> [Location]-> [Location] -> (a1, [Location])
+calcuPossibility candidates promisingLocation guess=
+    if noDistanceOf 0 guess promisingLocation then
         (3+ calculatePossibilityAll,guess)
     else
         (calculatePossibilityAll,guess)
-        where calculatePossibilityAll = calculatePossibilityOfOneGuess[feedback target guess| target <- guessCandidates]
+        where
+            calculatePossibilityAll =
+                possibilityOfOneGuess[feedback target guess| target <- candidates]
 
-calculatePossibilityOfOneGuess :: (Ord a2, Fractional a1) => [a2] -> a1
-calculatePossibilityOfOneGuess feedBacks = sum([fractionalDivide (length x * length x) (length feedBacks)| x <- group(sort feedBacks)])
-
-fractionalDivide :: (Fractional a1, Integral a2, Integral a3) => a2 -> a3 -> a1
-fractionalDivide a b = fromIntegral a / fromIntegral b
+-- This function is a helper function for "calculatePossibility",
+-- Input: a list of all feedbacks of one guess and each of its possible targets
+-- Output:  ∑ count(f)/2T
+possibilityOfOneGuess :: (Ord a2, Fractional a1) => [a2] -> a1
+possibilityOfOneGuess feedBacks =
+    sum([fractionalDivide (length x * length x) (length feedBacks)| x <- group(sort feedBacks)])
 
